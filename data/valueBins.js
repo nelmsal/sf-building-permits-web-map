67df19bde1331646df33d1ace9f07534030b642e
Alex Nelms <nelmsal@gmail.com>
2022-05-03 19:43:55 -0400

    boys, WE DID IT

---
+++ data/valueBins.js
@@ -0,0 +1,11 @@
+const valueBins = {
+    "units.tot.15_19": {
+        "value": "units.tot.15_19",
+        "bins": [0,1,10,25,50,100,200,10000],
+        "cmap": "Greens"
+    },
+    "units.tot.10_14": {
+        "value": "units.tot.15_19",
+        "bins": []
+    }
+}
